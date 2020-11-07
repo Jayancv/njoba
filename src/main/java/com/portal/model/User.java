a8b06e1f47dd30e283e54617d571f1edd62e4fb8
Jayancv <jayancv.13@cse.mrt.ac.lk>
2020-11-07 19:38:26 +0530

    Revert Revert "JCV : TeamMember IDs"

--- src/main/java/com/portal/model/User.java
+++ src/main/java/com/portal/model/User.java
@@ -35,7 +35,7 @@ import lombok.Setter;
 @Table(name="AD_USER")
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true, ignoreUnknown = true)
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "@id")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "@Id")
 public class User implements Serializable {
 
 	@Id @Getter @Setter 
